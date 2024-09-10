23a8671df9f48c4493c88d22917bbcd84a69388c
archTaha <aitabitaha@gmail.com>
2024-09-11 01:24:17 +0200

    created sound algo to grab relevant information from JSON

--- searchEngine/main.js
+++ searchEngine/main.js
@@ -7,62 +7,26 @@ searchInput.addEventListener("blur", (e) => {
 });
 searchInput.addEventListener("input", (e) => {
   searchQuery = e.target.value;
-  if (searchQuery !== '') {
-    searchRes.style.display = "flex";
-    searchRes.innerHTML = searchQuery
-  }
 });
-//Variables
-let searchQuery = '';
-let regionNames = [];
-let regionData = [];
-let regionSubdivs = [];
-let regionPrefectures = [];
-let regionProvinces = [];
-//Fetch
-fetch("../json/regions.json")
+fetch('../json/regions.json')
   .then(res => res.json())
   .then(data => {
     const regions = data.Regions;
-    //Level 1 keys:
-    //Region Names:
-    let rN = [];
-    Object.keys(rN.push(regions))[0]
-    //Pushes the keys containing the names of the regions to the globally accessibly array 'regionNames'.
-    pushKeys(rN,regionNames)
-    //Level 1 values:
-    let gON = [];
-    let sM = [];
-    gON.push(Object.values(regions)[0]['Guelmim-Oued Noun']);
-    pushEntries(gON, regionData);
-    sM.push(Object.values(regions)[1]['Sous-Massa']);
-    pushEntries(sM, regionData);
-    //Level 2 values:
-    let subDivs = [];
-    Object.keys(subDivs.push(gON[0][0].Subdivisions))
-    pushEntries(subDivs,regionSubdivs)
-    let prefectures = []
-    prefectures.push(regionSubdivs[0][0])
-    let provinces = []
-    provinces.push(regionSubdivs[0][1][1])
-    console.log(prefectures[0][1])
-
-    //Level 3 values:
+    const checkVariable = (arrVariable) => {
+      if (Array.isArray(arrVariable)) {
+        arrVariable.forEach(item => {
+          checkVariable(item);
+        })
+      } else if (typeof arrVariable === 'object' && arrVariable !== null) {
+        console.log(`Object = ${arrVariable}`)
+        for (const prop in arrVariable) {
+          if (arrVariable.hasOwnProperty(prop)) {
+            console.log(`Property ${prop} = ${arrVariable[prop]}`)
+            checkVariable(arrVariable[prop])
+          }
+        }
+      } else {
+        console.log(`${arrVariable} = ${typeof arrVariable}`);
+      }
+    }
   });
-
-// PUSHING DATA INTO ARRAYS:
-
-// Push region Names
-pushKeys = (array, globalArray) => {
-  for (let i = 0; i < array.length; i++) {
-    array[i].forEach(i => {
-      globalArray.push(Object.keys(i)[0])
-    });
-  };
-};
-// Push region Data: 
-pushEntries = (array, globalArray) => {
-  for (let i = 0; i < array.length; i++) {
-    globalArray.push(Object.entries(array[i][0]))
-  };
-};
