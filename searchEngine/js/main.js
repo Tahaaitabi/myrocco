7088cb0b288ce003ccf22f5b36cb93e7229580c1
Taha <aitabitaha@gmail.com>
2024-09-12 03:55:31 +0200

    +

--- searchEngine/js/main.js
+++ searchEngine/js/main.js
@@ -1,18 +1,20 @@
 //Elements
 const searchRes = document.getElementById('searchResults');
 const searchInput = document.getElementById('search-input');
+let query = '';
 //Event Handlers
 searchInput.addEventListener("blur", (e) => {
   searchRes.style.display = "none";
 });
 searchInput.addEventListener("input", (e) => {
-  searchQuery = e.target.value;
+  let query = e.target.value;
+
 });
 fetch('../json/regions.json')
   .then(res => res.json())
   .then(data => {
     const regions = data.Regions;
-    const checkVariable = (arrVariable) => {
+    /* const checkVariable = (arrVariable) => {
       if (Array.isArray(arrVariable)) {
         arrVariable.forEach(item => {
           checkVariable(item);
@@ -28,5 +30,20 @@ fetch('../json/regions.json')
       } else {
         console.log(`${arrVariable} = ${typeof arrVariable}`);
       }
+    } */
+
+    getObject = (arr) => {
+      if (Array.isArray(arr)) {
+        arr.forEach(item => {
+          for (const object in item) {
+            if (item.Name === 'Sidi-Ifni'){
+              console.log(item)
+              return
+            }
+            getObject(item[object])
+          }
+        })
+      } 
     }
+    getObject(regions)
   });
